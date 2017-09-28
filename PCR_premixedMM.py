80c6382231bbe5944cca57fca1c8a0e4d44b72e8
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-28 15:57:25 +0100

    WORKS

--- PCR_premixedMM.py
+++ PCR_premixedMM.py
@@ -2,17 +2,19 @@
 ## of a 96-well plate, add primers and add a given number of template samples.
 
 ###INPUT### PCR variables
-no_replicates = 8
-no_templates = 4
-no_primers = 2
-total_PCR_volume = 50
+num_replicates = 3
+num_templates = 2
+num_primers = 2
+
+total_PCR_volume = 25
+master_mix_volume = 10
+template_volume = 2
+primer_volume = 2.5
 
 from opentrons import robot, containers, instruments
 
 #Define containers - source_tube rack = cold block
-source_tubes = containers.load('tube-rack-2ml', 'E5', 'tube_rack')
 output = containers.load('96-PCR-flat', 'C1', 'output')
-
 trash = containers.load('trash-box', 'A3')
 
 #Create 6x12 p20 tip rack container
@@ -24,7 +26,8 @@ containers.create(
 	depth=60
 )
 
-p20rack = containers.load('tiprack-200ul-6x12', 'B2', 'p20_rack')
+p20rack1 = containers.load('tiprack-200ul-6x12', 'B3', 'p20_rack1')
+p20rack2 = containers.load('tiprack-200ul-6x12', 'B2', 'p20_rack2')
 
 #Create 3x6 2ml tube rack for DNA samples
 containers.create(
@@ -35,59 +38,57 @@ containers.create(
 	depth=40
 )
 
-DNA_rack = containers.load('3x6-tube-rack-2ml', 'C3', 'DNA_rack')
+template_rack = containers.load('3x6-tube-rack-2ml', 'E1', 'template_rack')
+source_tubes = containers.load('3x6-tube-rack-2ml', 'E3', 'tube_rack')
 
 #Define pipette
 p20 = instruments.Pipette(
     trash_container=trash,
-    tip_racks=[p20rack],
+    tip_racks=[p20rack1, p20rack2],
     min_volume=2,
     max_volume=20,
     axis="a"
 )
 
+#Define locations of PCR components
+water_source = source_tubes.wells('A1')
+master_mix_source = source_tubes.wells('A2')
+F_primer_source = source_tubes.wells('B2')
+R_primer_source = source_tubes.wells('C2')
+
 #Define DNA volumes to be added
-total_volume = 25
-DNA_volumes = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+template_volumes = [template_volume] * num_templates
 num_pcr_samples = len(DNA_volumes)
-DNA_sources = dna_tubes.wells('A1', length=num_pcr_samples)
-
-#Define locations of master mix components
-mix_location = source_tubes.wells('B1')
-water_source = source_tubes.wells('C1')
-
-#Distribute Master Mix
-p20.distribute(
-    12,
-    master_mix,
-    output.wells('A1', length=num_pcr_samples),
-    blow_out=True,
-    touch_tip=True,
-    new_tip='always'
-)
-
-#Add DNA
-p20.transfer(
-    DNA_volumes,
-    DNA_sources,
-    output.wells('A1', length=num_pcr_samples),
-    mix_after=(3, 4),
-    blow_out=True,
-    touch_tip=True,
-    new_tip='always'
-)
+template_sources = template_rack.wells('A1', length=num_pcr_samples)
+water_volume = total_PCR_volume - master_mix_volume - (2*primer_volume) - template_volume
 
 #Add water
-water_volumes = []
-for v in DNA_volumes:
-    water_volumes.append(total_volume - v - 12)
+for i in range(len(water_volumes)):
+	p20.transfer(
+		water_volume, water_source, output.wells(i*num_replicates, 
+		length=(num_replicates),skip=8), blow_out=True, touch_tip=True)
 
-p20.transfer(
-    water_volumes,
-    water_source,
-    output.wells('A1', length=num_pcr_samples),
-    mix_after=(3, 20),
-    blow_out=True,
-    touch_tip=True,
-    new_tip='always'
-)
+#distribute MasterMix
+for i in range(len(template_volumes)):
+	p20.transfer(
+		master_mix_volume, master_mix_source, output.wells(i*num_replicates, 
+		length=(num_replicates), skip=8), blow_out=True, touch_tip=True)
+		
+#distribute F primer
+for i in range(len(template_volumes)):
+	p20.transfer(
+		primer_volume, F_primer_source, output.wells(i*num_replicates, 
+		length=(num_replicates), skip=8), mix_after=(3, 3), blow_out=True, touch_tip=True, new_tip='always')
+
+#distribute R primer
+for i in range(len(template_volumes)):
+	p20.transfer(
+		primer_volume, R_primer_source, output.wells(i*num_replicates, 
+		length=(num_replicates), skip=8), mix_after=(3, 3), blow_out=True, touch_tip=True, new_tip='always')
+	
+#distribute template DNA
+for i in range(len(template_volumes)):
+	p20.transfer(
+		template_volumes[i], template_sources(i), output.wells(i*num_replicates, 
+		length=(num_replicates), skip=8), mix_after=(3, 20), blow_out=True, 
+		touch_tip=True, new_tip='always')
